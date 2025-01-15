c185c1eb267fa5c4acb0eb47f0fd311d49a74c85
zhiyuzheng1769 <jediknightnapoleon@gmail.com>
2025-01-15 11:58:52 +0000

    Update run_perm_sims.sh with correct julia script to run

--- distributed_diameter/run_perm_sims.sh
+++ distributed_diameter/run_perm_sims.sh
@@ -9,6 +9,6 @@ permstep=0.001
 for perm in $(seq ${permmin} ${permstep} ${permmax})
 	do
 	# use fsl_sub to submit a job described by the following arguments to a computing cluster, could be replaced with more general slurm commands
-	fsl_sub -q long "julia --project=. rng_distributed_perm_corr_D.jl --radius ${r_mean} --perm ${perm}"  # use the --project argument to specify the correct environment 
+	fsl_sub -q long "julia --project=. simulate_permeability.jl --radius ${r_mean} --perm ${perm}"  # use the --project argument to specify the correct environment 
 done
 
